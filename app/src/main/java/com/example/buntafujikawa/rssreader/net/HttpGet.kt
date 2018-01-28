9a9a4c5d32b13d621b720e7cbbde4d2a852b99dc
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-01-28 15:07:28 +0900

    add http url connection

---
+++ app/src/main/java/com/example/buntafujikawa/rssreader/net/HttpGet.kt
@@ -0,0 +1,51 @@
+package com.example.buntafujikawa.rssreader.net
+
+import java.io.BufferedInputStream
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+/**
+ * 指定したURLからダウンロードする
+ */
+class HttpGet(private val url: String, private var status: Int, private var input: InputStream) {
+
+    companion object {
+        private const val CONNECT_TIMEOUT_MS: Int = 3000
+        private const val READ_TIMEOUT_MS: Int = 5000
+    }
+
+    fun get(): Boolean {
+        try {
+            val url: URL = URL(this.url)
+
+            var connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+            connection.connectTimeout = CONNECT_TIMEOUT_MS
+            connection.readTimeout = READ_TIMEOUT_MS
+            connection.instanceFollowRedirects = true // リダイレクト許可
+
+            connection.connect()
+
+            status = connection.responseCode
+            if (status == 200) {
+                input = BufferedInputStream(connection.inputStream)
+                return true
+            }
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return false
+    }
+
+    fun getResponse(): InputStream {
+        return input
+    }
+
+    fun getStatus(): Int {
+        return status
+    }
+}
